d69772a1607cef82bb078daed716768583072770
SQUEEEE <elisabet@squeeee.se>
2016-07-13 22:32:52 +0200

    started player class

--- mugg.py
+++ mugg.py
@@ -1,3 +1,9 @@
+import random
+
+PLAYERS = 4
+playerList = []
+
+
 class Node:
 	def __init__(self,val):
 		self.value = val
@@ -30,10 +36,47 @@ class Stack:
 		return(self.top.value)
 
 
+class Player:
+	def __init__(self):
+		self.openDeck = []
+		self.closedDeck = []
+		self.accuracy = 1
 
-partDeck = list(range(1,16))
+	def __str__(self):
+		return "hej"
+		#s = "Open Deck: " + self.openDeck + "\n" + "Closed Deck: " + self.closedDeck + "\n" + "Accuracy: " + self.accuracy + "\n"
+		#return s
 
 
+def initGame():
+
+
+	#make deck
+
+	deck = []
+	partDeck = list(range(1,16))
+	counter = 0
+
+	while(counter<6):
+		deck.extend(partDeck)
+		counter+=1
+
+	random.shuffle(deck)
+
+	print(len(deck))
+	print(deck)
+
+
+
+	for i in range(0,PLAYERS):
+		playerList.append(Player)
+
+
+	for player in playerList:
+		print(player)
+
+
+initGame()
 
 '''
 
